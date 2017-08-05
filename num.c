f90c1f02a10156c29427dcb7ca79b1a246b22bc0
dhamodharan1996 <30342622+dhamodharan1996@users.noreply.github.com>
2017-08-05 16:42:19 +0530

    Create num.c

---
+++ num.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int j, i, sum = 0; 
+    printf("Enter a positive integer: ");
+    scanf("%d",&j);
+    for(i=1; i <= j; ++i)
+    {
+        sum += i;   // sum = sum+i;
+    }
+    printf("Sum = %d",sum);
+    return 0;
+}
